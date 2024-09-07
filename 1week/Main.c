674f10e0729fbeb953e547ad635d8567e6a074f0
Daniel_10110011 <shj1103s@naver.com>
2024-09-07 21:24:28 +0900

    update main.c

--- 1week/Main.c
+++ 1week/Main.c
@@ -10,17 +10,25 @@ int main(void)
     // printf("i = %d", i);
 
     // repaet 10 times
-    int number = 0;
+    int number[10];
+
     srand(time(NULL));
+
     for (int i = 0; i < 10; i++)
     {
-        number = rand();
-        printf("random_number = %d\n", number);
+        number[i] = (rand() % 100) + 1;
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("random_number = %d\n", number[i]);
     }
 
     // int rand() 와 void srand()의 차이점
     // > int rand가 동일한 seed를 가지고 0부터 RAND_MAX 사이의 의사 난수를 반환한다면
     // > void srand는 seed를 초기화한다. 보통 seed는 time(NULL)로 설정된다.
+
+    // number를 변수로 만드는 것보다, number[n]의 리스트 형태로 정의하면 할 수 있는 작업들이 많아진다.
 
     return 0;
 }
