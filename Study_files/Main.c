e8f8e44d719d6fc5ce6ddbf762fc86bf38ff5804
Daniel_10110011 <shj1103s@naver.com>
2024-09-08 16:43:58 +0900

    fix n update

--- Study_files/Main.c
+++ Study_files/Main.c
@@ -10,19 +10,19 @@ int main(void)
     // printf("i = %d", i);
 
     // repaet 10 times
-    int number[10];
+    // int number[10];
 
-    srand(time(NULL));
+    // srand(time(NULL));
 
-    for (int i = 0; i < 10; i++)
-    {
-        number[i] = (rand() % 100) + 1;
-    }
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     number[i] = (rand() % 100) + 1;
+    // }
 
-    for (int i = 0; i < 10; i++)
-    {
-        printf("random_number = %d\n", number[i]);
-    }
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     printf("random_number = %d\n", number[i]);
+    // }
 
     // int rand() 와 void srand()의 차이점
     // > int rand가 동일한 seed를 가지고 0부터 RAND_MAX 사이의 의사 난수를 반환한다면
@@ -30,5 +30,25 @@ int main(void)
 
     // number를 변수로 만드는 것보다, number[n]의 리스트 형태로 정의하면 할 수 있는 작업들이 많아진다.
 
+    int number[10];
+    int total = 0;
+    int manx, min;
+    float avg = 0.0;
+    srand(time(NULL));
+    for (int i=0; i<10; i++){
+        number[i] = (rand() % 100) + 1;
+    }
+    for(int i=0; i<10; i++){
+        printf("%3d\t", number[i]);
+        if((i+1) % 5 == 0) {
+            printf("\n");
+        }
+    }
+    for(int i=0; i<10; i++){
+        total += number[i];
+    }
+    avg = total / 10.0;
+    printf("%f", avg);
+    
     return 0;
 }
