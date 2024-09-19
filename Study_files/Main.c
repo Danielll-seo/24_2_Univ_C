4b7561225b1bc3aea608b0f4cb8ea96eeee3eede
Daniel_10110011 <shj1103s@naver.com>
2024-09-19 11:17:38 +0900

    update main.c

--- Study_files/Main.c
+++ Study_files/Main.c
@@ -133,11 +133,38 @@ int main(void)
     // main : a = 50, ch = 42;
     // test(&a, &ch);
 
-    int number[SIZE];
+    // int number[SIZE];
+    // srand(time(NULL));
+    // inputRandomArray(&number[0], SIZE);
+    // printArray(number, SIZE);
 
-    srand(time(NULL));
-    inputRandomArray(&number[0], SIZE);
-    printArray(number, SIZE);
+    // 쇼핑몰 재고수량관리
+    int num;
+    int Count;
+    
+    printf("입고할 상품의 개수(종류)를 입력하세요 > ");
+    scanf("%d", &Count);
+    
+    int Num_of_Product[Count];
+    int sell_product[Count];
+    
+    int b = sizeof(Num_of_Product) / sizeof(int);
+    int c = sizeof(sell_product) / sizeof(int);
+    printf("Num_of_Product > %d\n", b);
+    printf("sell_product %d\n", c);
+    
+    for(int i=0; i<Count; i++){
+        printf("%d번째 상품의 갯수를 입력 > ", i+1);
+        scanf("%d", &num);
+        Num_of_Product[i] = num;
+    }
+    
+    for(int i=0; i<Count; i++){
+        printf("%d번째 상품의 판매수량 입력 > ", i+1);
+        scanf("%d", &num);
+        sell_product[i] = num;
+        Num_of_Product[i] = Num_of_Product[i] - sell_product[i];
+    }
 
     return 0;
 }
