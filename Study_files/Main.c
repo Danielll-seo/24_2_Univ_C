d7b6d7950470991c5a3671125571140e790856ca
Daniel_10110011 <shj1103s@naver.com>
2024-10-16 21:57:10 +0900

    update main.c

--- Study_files/Main.c
+++ Study_files/Main.c
@@ -45,19 +45,19 @@ typedef struct pint{
     int y;
 }POINT;
 
-typedef struct circle{
-    POINT center;   // 원의 중심
-    double radius;  // 반지름
-}CIRCLE;
+// typedef struct circle{
+//     POINT center;   // 원의 중심
+//     double radius;  // 반지름
+// }CIRCLE;
 
-typedef struct rect{
-    POINT lb;   // left bottom
-    POINT rt;   // right top
-}RECT;
+// typedef struct rect{
+//     POINT lb;   // left bottom
+//     POINT rt;   // right top
+// }RECT;
 
-double dist(POINT p1, POINT p2){
-    return sqrt(pow((p2.x - p1.x),2)+pow((p2.y - p1.y),2));
-}
+// double dist(POINT p1, POINT p2){
+//     return sqrt(pow((p2.x - p1.x),2)+pow((p2.y - p1.y),2));
+// }
 
 int main(void)
 {
@@ -196,23 +196,36 @@ int main(void)
     //     printf("%c 출연횟수 : %d\n", 'a' + i, count[i]);
     // }
 
-    CIRCLE c1 = {{10, 10}, 4.5};
-    CIRCLE c2;
-    c2.radius = 5.5;
-    c2.center.x = 10;
-    c2.center.y = 20;
-    POINT point = {10,15};
-    double distance;
+    // CIRCLE c1 = {{10, 10}, 4.5};
+    // CIRCLE c2;
+    // c2.radius = 5.5;
+    // c2.center.x = 10;
+    // c2.center.y = 20;
+    // POINT point = {10,15};
+    // double distance;
 
-    printf("CIRCLE1 = (%d, %d), r : %lf\n", c1.center.x, c1.center.y, c1.radius);
-    printf("CIRCLE2 = (%d, %d), r : %lf\n", c2.center.x, c2.center.y, c2.radius);
-    printf("point = (%d, %d)\n", point.x, point.y);
+    // printf("CIRCLE1 = (%d, %d), r : %lf\n", c1.center.x, c1.center.y, c1.radius);
+    // printf("CIRCLE2 = (%d, %d), r : %lf\n", c2.center.x, c2.center.y, c2.radius);
+    // printf("point = (%d, %d)\n", point.x, point.y);
 
-    distance = dist(c1.center, point);
-    if(distance <= c1.radius){
-        printf("원 안에 있습니다.\n");
-    }else{
-        printf("원 밖체 있습니다.\n");
+    // distance = dist(c1.center, point);
+    // if(distance <= c1.radius){
+    //     printf("원 안에 있습니다.\n");
+    // }else{
+    //     printf("원 밖체 있습니다.\n");
+    // }
+
+    POINT point[SIZE] = {0};
+    POINT temp = {0, 0};
+    int least;
+
+    srand(time(NULL));
+    for(int i=0; i<SIZE; i++){
+        point[i].x = rand() % 101;
+        point[i].y = rand() % 101;
+    }
+    for(int i=0; i<SIZE; i++){
+        printf("point[%d] : (%d, %d)\n", i, point[i].x, point[i].y);
     }
 
     return 0;
