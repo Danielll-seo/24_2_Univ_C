49eca2d687fd1f15ecb2795914c3a4400e19da87
Daniel_10110011 <shj1103s@naver.com>
2024-11-18 22:51:19 +0900

    Update Main.c

--- Study_files/Main.c
+++ Study_files/Main.c
@@ -141,7 +141,7 @@ void loadFILE(POINT p[], int size){
 }
 */
 
-// 11주차 동적할당 활용 1
+// 11주차 동적할당 활용 1~2(함수화, 메뉴추가)
 typedef struct point{
     int x;
     int y;
@@ -152,6 +152,25 @@ int inputMenu(void){
     printf("메뉴입력(1.구조체 만들기, 2. 구조체 출력하기, -1. 종료) : ");
     scanf("%d", &input);
     return input;
+}
+
+POINT* makePoint(void){
+    POINT* p = (POINT*)malloc(sizeof(POINT));
+        p->x = rand() % 100;
+        p->y = rand() % 100; 
+        return p;
+}
+
+void printPoint(POINT* p[], int size){
+    for(int i=0; i<size; i++){
+        printf("x:%d, y:%d\n", p[i]->x, p[i]->y);
+    }
+}
+
+void freePoints(POINT* p[], int size){
+    for(int i=0; i<size; i++){
+        free(p[i]);
+    }
 }
 
 int main()
@@ -403,11 +422,25 @@ int main()
     free(pInt);
     */
 
-    // 11주차 동적할당 활용 1
+    // 11주차 동적할당 활용 1~3(함수화, 메뉴추가)
     int menu;
+    int count;
+    POINT* points[100];
 
     do{
         menu = inputMenu();
+        if(menu == 1){
+            // 구조체 만들기
+            points[count] = makePoint();
+            count++;
+            printf("만들었습니다.");
+        }else if(menu == 2){
+            // 구조체 출력하기
+            printPoint(points, count);
+        }else if(menu == -1){
+            freePoints(points, count);
+            printf("프로그램을 종료합니다.\n");
+        }
     }while(menu > 0);
 
     /*
